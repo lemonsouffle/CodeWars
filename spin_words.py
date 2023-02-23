94a41cb72b57a786b8c6d017d5910ee7033fcca8
lemonsouffle <105547281+lemonsouffle@users.noreply.github.com>
2023-02-23 15:16:42 +0300

    Update spin_words.py

--- spin_words.py
+++ spin_words.py
@@ -10,7 +10,8 @@
 # spinWords( "This is another test" )=> returns "This is rehtona test"
 
 def spin_words(sentence):
-    for i in sentence.split(' '):
+    my_list = sentence.split(' ')
+    for i in my_list:
         my_list[my_list.index(i)] = i[::-1] if len(i) >= 5 else i
     return ' '.join(my_list)
 
